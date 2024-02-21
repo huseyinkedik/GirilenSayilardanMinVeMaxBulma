fc5adfff839f1e6cae4aee67d16de13c992c9380
huseyinkedik <159181717+huseyinkedik@users.noreply.github.com>
2024-02-21 10:15:58 +0300

    Düzenleme-1

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.print("Kaç tane sayı girişi yapılacak: ");
         int n = input.nextInt();
-
-        //Döngüyü 1 olarak başlatıyoruz
         int c = 1;
-
         System.out.print(" 1 nolu sayıyı giriniz: ");
         int ilkDeger = input.nextInt();
         int enBuyukSayi = ilkDeger;
-        int enKucukSayi = ilkDeger;
 
-        {
-            while (c < n) {
-                System.out.print(c + 1 + ". nolu sayıyı giriniz: ");
-                int yenideger = input.nextInt();
-
-                if (yenideger > enBuyukSayi) {
-                    enBuyukSayi = yenideger;
-                } else {
-                    enKucukSayi = yenideger;
-                }
-                c++;
+        int enKucukSayi;
+        for(enKucukSayi = ilkDeger; c < n; ++c) {
+            System.out.print(c + 1 + ". nolu sayıyı giriniz: ");
+            int a = input.nextInt();
+            if (a > enBuyukSayi) {
+                enBuyukSayi = a;
+            } else {
+                enKucukSayi = a;
             }
-            System.out.println("En büyük sayı: " + enBuyukSayi);
-            System.out.println("En küçük sayı: " + enKucukSayi);
         }
+
+        System.out.println("En büyük sayı: " + enBuyukSayi);
+        System.out.println("En küçük sayı: " + enKucukSayi);
     }
 }
